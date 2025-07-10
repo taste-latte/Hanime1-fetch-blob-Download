33254ec420a27ac6608cd4f4ee28aab9c6df6352
taste-latte <qaz323456655@gmail.com>
2025-07-11 06:26:37 +0800

    Update Hanime1 [fetch + blob] Download.meta.js

--- Hanime1 [fetch + blob] Download.meta.js
+++ Hanime1 [fetch + blob] Download.meta.js
@@ -2,7 +2,7 @@
 // @name         Hanime1 [fetch + blob] Download
 // @namespace    http://tampermonkey.net/
 // @version      1.1
-// @description  下載按鈕放訂閱旁，自動正確命名影片[使用(fetch + blob)強制命名]
+// @description  下載按鈕放訂閱旁，自動正確命名影片[使用(fetch + blob)強制命名]，紀錄下載的影片並且下次下載相同影片會提示已下載是否下載
 // @match        *://hanime1.me/*
 // @updateURL     https://github.com/taste-latte/Hanime1-fetch-blob-Download/tree/main/Hanime1 [fetch + blob] Download.meta.js
 // @downloadURL   https://github.com/taste-latte/Hanime1-fetch-blob-Download/tree/main/Hanime1 [fetch + blob] Download.user.js
