13f6bd4e431baa14885a7ff1bdb561497eafab5e
taste-latte <qaz323456655@gmail.com>
2025-07-11 05:53:34 +0800

    Add files via upload
    v1.0

---
+++ Hanime1 [fetch + blob] Download.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Hanime1 [fetch + blob] Download
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  下載按鈕放訂閱旁，自動正確命名影片[使用(fetch + blob)強制命名]
+// @match        *://hanime1.me/*
+// @updateURL     https://raw.githubusercontent.com/inDream/DownAlbum/master/Hanime1%20%5Bfetch%20%2B%20blob%5D%20Download-1.0.meta.js
+// @downloadURLhttps://github.com/taste-latte/Hanime1-fetch-blob-Download/blob/main/Hanime1%20%5Bfetch%20%2B%20blob%5D%20Download-1.0.user.js
+// @grant        none
+// ==/UserScript==
