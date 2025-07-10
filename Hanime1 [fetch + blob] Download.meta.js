5dbef83cd3ff552dd0579921c35521433937416c
taste-latte <qaz323456655@gmail.com>
2025-07-11 06:26:10 +0800

    Update Hanime1 [fetch + blob] Download.meta.js

--- Hanime1 [fetch + blob] Download.meta.js
+++ Hanime1 [fetch + blob] Download.meta.js
@@ -4,7 +4,7 @@
 // @version      1.1
 // @description  下載按鈕放訂閱旁，自動正確命名影片[使用(fetch + blob)強制命名]
 // @match        *://hanime1.me/*
-// @updateURL     https://raw.githubusercontent.com/inDream/DownAlbum/master/Hanime1%20%5Bfetch%20%2B%20blob%5D%20Download-1.0.meta.js
-// @downloadURLhttps://github.com/taste-latte/Hanime1-fetch-blob-Download/blob/main/Hanime1%20%5Bfetch%20%2B%20blob%5D%20Download-1.0.user.js
+// @updateURL     https://github.com/taste-latte/Hanime1-fetch-blob-Download/tree/main/Hanime1 [fetch + blob] Download.meta.js
+// @downloadURL   https://github.com/taste-latte/Hanime1-fetch-blob-Download/tree/main/Hanime1 [fetch + blob] Download.user.js
 // @grant        none
 // ==/UserScript==
