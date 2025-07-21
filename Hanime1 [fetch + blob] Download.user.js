c21a4a271bf71b495664e7f9e3c98d39137d3937
taste-latte <qaz323456655@gmail.com>
2025-07-21 14:56:24 +0800

    Update Hanime1 [fetch + blob] Download.user.js

--- Hanime1 [fetch + blob] Download.user.js
+++ Hanime1 [fetch + blob] Download.user.js
@@ -2,6 +2,8 @@
 // @name         Hanime1 [fetch + blob] Download + 管理清單(暫時只顯示圖示)
 // @namespace    http://tampermonkey.net/
 // @version      1.56
+// @updateURL    https://github.com/taste-latte/Hanime1-fetch-blob-Download/raw/refs/heads/main/Hanime1%20%5Bfetch%20+%20blob%5D%20Download.meta.js
+// @downloadURL  https://github.com/taste-latte/Hanime1-fetch-blob-Download/raw/refs/heads/main/Hanime1%20%5Bfetch%20+%20blob%5D%20Download.user.js
 // @description  (功能)使用 videoId 精準紀錄已下載影片，下載影片自動命名，管理清單支援單筆刪除與固定清除按鈕以及匯出與匯入紀錄。
 // @description  (更新)下載紀錄清單搜尋時會凸顯搜尋文字，搜尋時會從整個清單中找尋影片名稱，下載紀錄清單新增影片圖示可以跳轉到該影片觀看網頁
 // @description  (待修正)下載清單暫時只有圖示(加上文字會使影片縮圖變小)
